4264c05863ea0ce28de02b4a328a91b5edf2ba1a
plasalt <22253@gdc.school.nz>
2025-08-28 15:19:54 +1200

    changed the formatting for the grid and buttons to make it look nice

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ root.geometry("670x500")
 
 #function
 def mainmenusetup():
-    mainframe = tk.Frame(root,bg="#1f57a2",width=670,height=500)
-    mainframe.pack(fill="both",expand=True)
+    mainframe = tk.Frame(root,bg="#1f57a2",width=670,height=220)
+    mainframe.pack(side="top")
+    main2frame = tk.Frame(root,bg="#1f57a2",width=670,height=0)
+    main2frame.pack(fill="both",expand=True)
 
     title = tk.Label(mainframe,text="Monster Card Catalogue",bg="#1f57a2",font=("Anton",30,"bold"))
     title.grid(row=0,column=0,columnspan=5)
@@ -25,23 +27,23 @@ def mainmenusetup():
     for row in range(2):
         for col in range(5):
             print("bleh")
-            hold = tk.Button(mainframe, text=f"monster{i+1}",width=12,height=5,bg="#a6a6a6",command=lambda idx=i: buttonmonsters(idx))
+            hold = tk.Button(mainframe, text=f"monster{i+1}",width=12,height=5,bg="#a6a6a6",command=lambda idx=i: buttonmonsters(idx),relief='flat',font=("Anton",8,"bold"))
             hold.grid(row=row+2, column=col,padx=20,pady=10)
             buttons.append(hold)
             
             i += 1 
-    addcard = tk.Button(mainframe,text="add card",width=10,height=1)
-    addcard.grid(row=4,column=0)
-    removecard = tk.Button(mainframe,text="add card",width=10,height=1)
-    removecard.grid(row=4,column=1)
-    editcard = tk.Button(mainframe,text="add card",width=10,height=1)
-    editcard.grid(row=4,column=2)
-    printcard = tk.Button(mainframe,text="add card",width=10,height=1)
-    printcard.grid(row=4,column=3)
-    selectedcard = tk.Label(mainframe,text="selected card")
-    selectedcard.grid(row=4,column=4)
-    viewcard = tk.Button(mainframe,text="add card",width=10,height=1)
-    viewcard.grid(row=4,column=5)
+    addcard = tk.Button(main2frame,text="add card",width=10,height=1,relief='flat',font=("Anton",8,"bold"))
+    addcard.grid(row=-0,column=0,padx=20,pady=10)
+    removecard = tk.Button(main2frame,text="remove card",width=10,height=1,relief='flat',font=("Anton",8,"bold"))
+    removecard.grid(row=0,column=1,padx=14,pady=10)
+    editcard = tk.Button(main2frame,text="edit card",width=10,height=1,relief='flat',font=("Anton",8,"bold"))
+    editcard.grid(row=0,column=2,padx=14,pady=10)
+    printcard = tk.Button(main2frame,text="view card",width=10,height=1,relief='flat',font=("Anton",8,"bold"))
+    printcard.grid(row=0,column=3,padx=14,pady=10)
+    selectedcard = tk.Label(main2frame,text="selected card",font=("Anton",8,"bold"))
+    selectedcard.grid(row=0,column=4,padx=14,pady=10)
+    viewcard = tk.Button(main2frame,text="view card",width=10,height=1,relief='flat',font=("Anton",8,"bold"))
+    viewcard.grid(row=0,column=5,padx=14,pady=10)
 
 def buttonmonsters(idx):
     print(idx,"pressed")
